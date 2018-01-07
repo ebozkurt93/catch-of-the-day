6607f4189d22dea9a980c820884378e950cee6d5
Erdem Bozkurt <ebozkurt@sabanciuniv.edu>
2018-01-08 01:37:15 +0300

    video 14

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
     constructor() {
         super();
 
         this.addFish = this.addFish.bind(this);
+        this.loadSamples = this.loadSamples.bind(this);
         this.state = {
             fishes: {},
             order: {} 
@@ -21,7 +23,14 @@ const fishes = {...this.state.fishes};
 const timestamp = Date.now();
 fishes[`fish-${timestamp}`] = fish;
 //set state
-this.setState({ fishes})
+this.setState({ fishes});
+}
+
+loadSamples() {
+    this.setState({
+        fishes: sampleFishes
+    });
+        
 }
 
 
@@ -32,7 +41,7 @@ this.setState({ fishes})
                     <Header tagline="Fresh Seafood Market"/>
                 </div>
                 <Order />
-                <Inventory addFish={this.addFish} />
+                <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
             </div>
         )
     }
