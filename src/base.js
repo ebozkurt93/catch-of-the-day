3adc169a150424cb61f966601ba95f08d646bcc3
Erdem Bozkurt <ebozkurt@sabanciuniv.edu>
2018-01-29 18:23:38 +0300

    video 18

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyAQvSCtGgLs7r0rTal5DYLOs9g0DsyteI0",
+    authDomain: "catch-of-the-day-eaa5c.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-eaa5c.firebaseio.com",
+});
+
+export default base;
